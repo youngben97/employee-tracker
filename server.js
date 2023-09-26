bfb51a83b779dc1f8ca2f00e34708bb9547fa203
GreatGaspargoo <b.c.young.97@gmail.com>
2023-09-25 18:10:51 -0600

    added server file and schema

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require(mysql2);
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: '127.0.0.1',
+        user: 'root',
+        password:'',
+        database: 'employee_db'
+    }
+)
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
